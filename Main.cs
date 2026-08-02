75fc2c32da27b5c3e17d4fd9e74a20f74a2ee8b0
cofBy <foxmagic24@gmail.com>
2026-08-02 15:48:22 +0300

    square
    it now draws a square with 4 vertices and 2 triangles

--- Main.cs
+++ Main.cs
@@ -18,13 +18,19 @@ namespace VoxelRenderer
         }
 
         float[] vertices = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f 
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left
+        };
+        uint[] indices = {
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
         };
 
         int VertexBufferObject;
         int VertexArrayObject;
+        int ElementBufferObject;
 
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
@@ -51,6 +57,10 @@ namespace VoxelRenderer
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
             GL.EnableVertexAttribArray(0);
 
+            ElementBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+
             shader = new Shader("Z:/projectCof/VoxelRenderer/VoxelRenderer/shader.vert", "Z:/projectCof/VoxelRenderer/VoxelRenderer/shader.frag");
             shader.Use();
         }
@@ -69,7 +79,7 @@ namespace VoxelRenderer
 
             shader.Use();
             GL.BindVertexArray(VertexArrayObject);
-            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+            GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
 
             SwapBuffers();
         }
